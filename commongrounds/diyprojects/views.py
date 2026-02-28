0cfe1b68bd83bb36ac7937273f6997196e4ad232
Elijah Tan <elijahtan1179@gmail.com>
2026-02-28 20:33:32 +0800

    Add list and detail views.

--- commongrounds/diyprojects/views.py
+++ commongrounds/diyprojects/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from .models import ProjectCategory, Project
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = ProjectCategory
+    template_name = 'diyprojects/project_list.html'
+
+    def get_queryset(self):
+        return ProjectCategory.objects.prefetch_related('project_set').all()
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'diyprojects/project_detail.html'
