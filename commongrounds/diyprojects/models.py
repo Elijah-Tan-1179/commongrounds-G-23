2b418f7f8722c374b39146d934490efccb67d510
Elijah Tan <elijahtan1179@gmail.com>
2026-02-28 17:56:29 +0800

    Create models.

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class ProjectCategory(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name        
+    
+    class Meta:
+        ordering = ['name']
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    materials = models.TextField()
+    steps = models.TextField()
+    created_on = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return f"{self.title} created on {self.created_on}."
+    
+    class Meta:
+        ordering = ['-created_on']
