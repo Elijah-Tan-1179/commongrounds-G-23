3f66278cf191723ffc0547b79262aabc2deb1a6c
Elijah Tan <elijahtan1179@gmail.com>
2026-02-28 21:28:55 +0800

    Add admin panels.

--- commongrounds/diyprojects/admin.py
+++ commongrounds/diyprojects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import ProjectCategory, Project
 
-# Register your models here.
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    model = ProjectCategory
+
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
