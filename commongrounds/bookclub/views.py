137d98cc5ee582be5b202936b60eb61c97287a9b
Zachary <ong.zach13@gmail.com>
2026-03-01 16:22:07 +0800

    Added views.py, which is responsible for rendering what the user sees, specifically the book list and the individual books. Also added comments and docstrings for this one

---
+++ commongrounds/bookclub/views.py
@@ -0,0 +1,33 @@
+from django.views.generic import ListView, DetailView
+from .models import Book
+
+class BookListView(ListView):
+    """
+    Renders a list of all books stored in the database. 
+
+    Context:
+        books: A query of all books, ordered by publication
+        year. 
+
+    Template:
+        bookclub/book_list.html
+    """
+    model = Book # used as wrapper for book metadata
+    template_name = 'bookclub/book_list.html'
+    context_object_name = 'books'
+
+class BookDetailView(DetailView): 
+    """
+    Renders the information for a specific book. 
+
+    Context: 
+        book: The specific book, which is identified by the URL
+        primary key
+
+    Template:
+        bookclub/book_detail.html
+    """
+    model = Book
+    template_name = 'bookclub/book_detail.html'
+    context_object_name = 'book'
+
