2f7c4e08b793b95ba5313464f64031ac1515b32d
EDPBsCs <edward.albert.ii.prades@student.ateneo.edu>
2026-03-10 01:41:59 +0800

    Immplemented models into view.py

--- commongrounds/commissions/views.py
+++ commongrounds/commissions/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Commission
 
-# Create your views here.
+# List view for commission model
+def commission_list(request):
+
+    commissions = Commission.objects.all()
+
+    context = {
+        "commissions": commissions,
+    }
+
+    return render(
+        request,
+        "commissions/commission_list.html",
+        context,
+    )
+
+# Detail view for commission model
+def commission_detail(request, pk):
+
+    commission = get_object_or_404(
+        Commission,
+        pk=pk,
+    )
+
+    context = {
+        "commission": commission,
+    }
+
+    return render(
+        request,
+        "commissions/commission_detail.html",
+        context,
+    )
