22ff4656b0505e7df5ae5fb42b3f0a9000ae3af5
EDPBsCs <edward.albert.ii.prades@student.ateneo.edu>
2026-03-10 01:39:52 +0800

    REgisted models into admin

--- commongrounds/commissions/admin.py
+++ commongrounds/commissions/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Commission, CommissionType
+
+# register com type model
+@admin.register(CommissionType)
+class CommissionTypeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+# register com req model
+@admin.register(Commission)
+class CommissionAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "people_required",
+        "created_on",
+    )
+
+    search_fields = ("title",)
