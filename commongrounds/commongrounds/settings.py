923de924c859bdb7612b6d4604e4d3ef6e5d4855
Elijah Tan <elijahtan1179@gmail.com>
2026-03-10 22:53:53 +0800

    Uncomment all installed apps and urls

--- commongrounds/commongrounds/settings.py
+++ commongrounds/commongrounds/settings.py
@@ -37,10 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'merchstore',
-    #'localevents',
+    'merchstore',
+    'localevents',
     'bookclub',
-    #'diyprojects',
+    'diyprojects',
     "commissions",
 ]
 
