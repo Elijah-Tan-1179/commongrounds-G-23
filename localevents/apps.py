9d59a9f3aaa82b0af5071b9613e1d74b3ec419d6
Elijah Tan <elijahtan1179@gmail.com>
2026-03-10 22:51:17 +0800

    Remove redundant/unnecessary files

--- localevents/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LocaleventsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "localevents"
--- localevents/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'localevents'
-
-urlpatterns = [
-    path('events', views.event_list, name='event_list'),
-    path('event/<int:pk>', views.event_detail, name='event_detail'),
-]
