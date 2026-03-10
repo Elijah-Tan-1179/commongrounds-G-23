6450952a52188f4d8975d4365a131f36d206bcb3
nikolaipascual-design <nikolai.pascual@student.ateneo.edu>
2026-03-10 18:37:27 +0800

    Adjust to PEP8 standards

--- commongrounds/merchstore/models.py
+++ commongrounds/merchstore/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2,max_digits=10)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
 
     def __str__(self):
         return self.name
