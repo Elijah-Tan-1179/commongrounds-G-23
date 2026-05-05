ca8b30888fe1fcf147858fc2b31df0ea9ec38b60
Elijah Tan <elijahtan1179@gmail.com>
2026-05-05 15:40:23 +0800

    Update models and urls

--- commongrounds/diyprojects/models.py
+++ commongrounds/diyprojects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class ProjectCategory(models.Model):
@@ -16,6 +17,7 @@ class ProjectCategory(models.Model):
 class Project(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(ProjectCategory, on_delete=models.SET_NULL, null=True)
+    creator = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
     description = models.TextField()
     materials = models.TextField()
     steps = models.TextField()
@@ -28,3 +30,31 @@ class Project(models.Model):
     class Meta:
         ordering = ['-created_on']
         verbose_name_plural = "projects"
+
+
+class Favorite(models.Model):
+    project_status_choices = [
+        ('B', 'Backlog'),
+        ('T', 'To-Do'),
+        ('D', 'Done')
+    ]
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    date_favorited = models.DateField()
+    project_status = models.CharField(max_length=7, choices=project_status_choices)
+
+
+class ProjectReview(models.Model):
+    reviewer = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    comment = models.TextField()
+    image = models.ImageField()
+
+
+class ProjectRating(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ]
+    )
