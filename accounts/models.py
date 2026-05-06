30621bb9e6bcae3f4a5e4e1e0776a9c45965174f
Gregory Wilford <edward.albert.ii.prades@student.ateneo.edu>
2026-05-06 09:56:11 +0800

    Double-checked fille contents of others and fixed a few names

--- accounts/models.py
+++ accounts/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# THIS IS JUST A PLACEHOLDER FOR THINGS THAT I USE, IF ALREADY IMPLEMENTED CAN YOU REMOVE THIS PORTION OF MINES AND RECHANGE SOME VARIABLE NAMES
+# REMOVE THE ACCOUNTS APP FOR MINES HWNE TESTING
 class Profile(models.Model):
-    # Link to the built-in Django User
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     
-    # Required by specs: Display name (max 63 chars) and Email
     display_name = models.CharField(max_length=63)
     email = models.EmailField()
     
-    # Required for Advanced Requirements: Role-based access
     ROLE_CHOICES = [
         ('Market Seller', 'Market Seller'),
         ('Event Organizer', 'Event Organizer'),
