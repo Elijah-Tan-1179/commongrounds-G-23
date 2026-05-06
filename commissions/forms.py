cd1fe8f697bdb46b0c25170d28f2d70e600ab2e7
Gregory Wilford <edward.albert.ii.prades@student.ateneo.edu>
2026-05-06 09:54:02 +0800

    readded forms.py after realizing it was removed by an AV

---
+++ commissions/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Commission, Job, JobApplication
+
+class CommissionForm(forms.ModelModel):
+    class Meta:
+        model = Commission
+        # Maker is excluded because it is set automatically in the view
+        fields = ['title', 'description', 'type', 'people_required', 'status']
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Job
+        fields = ['role', 'manpower_required', 'status']
+
+#callows multiple Jobs to be edited on the same page as the Commission
+JobFormSet = forms.inlineformset_factory(
+    Commission, Job, form=JobForm, extra=1, can_delete=True
+)
+
+class JobApplicationForm(forms.ModelForm):
+    class Meta:
+        model = JobApplication
+        fields = [] # applicant is the logged in user
