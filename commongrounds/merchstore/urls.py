32ccf63b646c9f8c6066d4a46b642416d4264cce
nikolaipascual-design <nikolai.pascual@student.ateneo.edu>
2026-05-07 23:02:42 +0800

    Added urls

--- commongrounds/merchstore/urls.py
+++ commongrounds/merchstore/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.shortcuts import redirect
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, CartView, TransactionListView
 
 
 urlpatterns = [
     path('', lambda request: redirect('product_list')),
-    path('products', ProductListView.as_view(), name='product_list'),
-    path('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('items', ProductListView.as_view(), name='product_list'),
+    path('item/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path('item/add', ProductCreateView.as_view(), name='product_create'),
+    path('item/<int:pk>/edit', ProductUpdateView.as_view(), name='product_update'),
+    path('cart', CartView.as_view(), name='product_cart'),
+    path('transactions', TransactionListView.as_view(), name='product_transaction'),
 ]
