1d29b23a32f7054c09a35a5f2ba179cf7975d31f
Gregory Wilford <edward.albert.ii.prades@student.ateneo.edu>
2026-05-07 23:31:38 +0800

    Redone admin (temporary)

--- commongrounds/commissions/admin.py
+++ commongrounds/commissions/admin.py
@@ -1,37 +1,13 @@
 from django.contrib import admin
-from .models import CommissionType, Commission, Job, JobApplication
+from .models import (
+    CommissionType,
+    Commission,
+    Job,
+    JobApplication
+)
 
 
-@admin.register(CommissionType)
-class CommissionTypeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description']
-
-
-class JobInline(admin.StackedInline):
-    model = Job
-    extra = 1
-
-
-class JobApplicationInline(admin.StackedInline):
-    model = JobApplication
-    extra = 0
-
-
-@admin.register(Commission)
-class CommissionAdmin(admin.ModelAdmin):
-    list_display = ['title', 'type', 'maker', 'people_required', 'status', 'created_on']
-    list_filter = ['status', 'type']
-    inlines = [JobInline]
-
-
-@admin.register(Job)
-class JobAdmin(admin.ModelAdmin):
-    list_display = ['role', 'commission', 'manpower_required', 'status']
-    list_filter = ['status']
-    inlines = [JobApplicationInline]
-
-
-@admin.register(JobApplication)
-class JobApplicationAdmin(admin.ModelAdmin):
-    list_display = ['job', 'applicant', 'status', 'applied_on']
-    list_filter = ['status']
+admin.site.register(CommissionType)
+admin.site.register(Commission)
+admin.site.register(Job)
+admin.site.register(JobApplication)
