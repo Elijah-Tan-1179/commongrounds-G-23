74917d5e65b8aad28d471a78f14b594ad7b1c39e
nikolaipascual-design <nikolai.pascual@student.ateneo.edu>
2026-05-07 23:01:24 +0800

    Added product update form

---
+++ commongrounds/merchstore/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import Transaction, Product
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount']
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name',
+                'product_type',
+                'product_image',
+                'description',
+                'price',
+                'stock',
+                'status']
+        widgets = {
+            'status': forms.Select(),
+            'product_type': forms.Select(),
+        }
+
+class ProductUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name',
+                'product_type',
+                'product_image',
+                'description',
+                'price',
+                'stock',
+                'status']
+        widgets = {
+            'status': forms.Select(),
+            'product_type': forms.Select(),
+        }
